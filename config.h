984bb9688a0d73fd705a61fa4379baba9e24e08a
EvaristeFan <reluE_L@outlook.com>
2022-04-07 18:30:03 +0800

    Change the scrathpad's size for terminal

--- config.h
+++ config.h
@@ -40,7 +40,7 @@ typedef struct {
 	const char *name;
 	const void *cmd;
 } Sp;
-const char *spcmd1[] = {"st", "-n", "spterm", "-g", "120x34", NULL };
+const char *spcmd1[] = {"st", "-n", "spterm", "-g", "135x38", NULL };
 const char *spcmd2[] = {"st", "-n", "spfm", "-g", "144x41", "-e", "ranger", NULL };
 const char *spcmd3[] = {"keepassxc", NULL };
 static Sp scratchpads[] = {
